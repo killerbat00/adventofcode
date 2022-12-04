a9a9d509401b20a7ad3ef9ace3588d97204bf7ff
brian houston morrow <bhm@brianmorrow.net>
2022-12-03 20:30:00 -0600

    Day 3 Part 2.

--- day_3.nim
+++ day_3.nim
@@ -23,7 +23,27 @@ proc partOne() =
     echo "Part one: ", totalScore
 
 proc partTwo() =
-    echo "Part two: "
+    var
+        idx = 0
+        totalScore = 0
+        peerGroup = newSeq[string]()
+
+    withFile(f, "./input/day_3.txt", fmRead):
+        for line in lines(f):
+            if line != "":
+                peerGroup.add(line)
+                idx = (idx + 1) mod 3
+                if idx == 0:
+                    let
+                        common = intersection(intersection(peerGroup[0].toHashSet(), peerGroup[1].toHashSet()), peerGroup[2].toHashSet()).toSeq()[0]
+
+                    if common.isUpperAscii():
+                        totalScore += int(common) - 38
+                    else:
+                        totalScore += int(common) - 96
+                    peerGroup.setLen(0)
+
+    echo "Part two: ", totalScore
 
 when isMainModule:
     partOne()
