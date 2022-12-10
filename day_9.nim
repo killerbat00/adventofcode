b0c2e3cb3593abd88348bf07c1522849fc787c5b
brian houston morrow <bhm@brianmorrow.net>
2022-12-09 19:27:00 -0600

    Day 9 Part 2.

--- day_9.nim
+++ day_9.nim
@@ -1,8 +1,8 @@
 from utils import withStream
 from streams import lines
 import strutils
-import strformat
 import sets
+import sugar
 
 const TEST_DATA = """
 R 4
@@ -15,22 +15,31 @@ L 5
 R 2
 """
 
+const TEST_DATA_2 = """
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+"""
+
 type
     Point = object
         x: int
         y: int
-
-var pHead = Point(x: 0, y: 0)
-var pTail = Point(x: 0, y: 0)
-
-var tailMoves: HashSet[Point] = initHashSet[Point]()
 
 func touching(head: Point, tail: Point): bool =
     if abs(head.x - tail.x) <= 1 and abs(head.y - tail.y) <= 1:
         return true
     return false
 
-proc singleMove(direction: string) =
+proc `$`(p: Point): string =
+    return "Point(x: " & $p.x & ", y: " & $p.y & ")"
+
+proc singleMove(direction: string, pHead: var Point, pTail: var Point, tailMoves: var HashSet[Point], isTail: bool = false) =
     case direction:
         of "R":
             pHead.x += 1
@@ -41,9 +50,7 @@ proc singleMove(direction: string) =
         of "D":
             pHead.y += 1
 
-    if touching(pHead, pTail):
-        return
-    
+proc catchUp(pHead: var Point, pTail: var Point, tailMoves: var HashSet[Point], isTail: bool = false) =
     if pHead.x == pTail.x:
         if pHead.y - pTail.y == 2:
             # head is further down, tail needs to move down one
@@ -77,8 +84,8 @@ proc singleMove(direction: string) =
                 # head is further up and left, tail needs to move up and left
                 pTail.x -= 1
                 pTail.y -= 1
-    #echo fmt"Head is at {pHead.x}, {pHead.y}. Tail is at {pTail.x}, {pTail.y}"
-    tailMoves.incl(Point(x: pTail.x, y: pTail.y))
+    if isTail:
+        tailMoves.incl(Point(x: pTail.x, y: pTail.y))
 
 proc partOne() =
     let fn = "./input/day_9.txt"
@@ -91,27 +98,43 @@ proc partOne() =
             let parts = line.split(" ")
             moveMap.add((dir: parts[0], amt: parseInt(parts[1])))
 
-    pHead = Point(x: 0, y: 0)
-    pTail = Point(x: 0, y: 0)
-    tailMoves = initHashSet[Point]()
+    var pHead = Point(x: 0, y: 0)
+    var pTail = Point(x: 0, y: 0)
+    var tailMoves = initHashSet[Point]()
     for move in moveMap:
         for i in 0 ..< move.amt:
-            singleMove(move.dir)
+            singleMove(move.dir, pHead, pTail, tailMoves, true)
+            if not touching(pHead, pTail):
+                catchUp(pHead, pTail, tailMoves, true)
 
     echo "Part one: ", tailMoves.len + 1 #count tail starting position
 
 proc partTwo() =
-    #let fn = "./input/day_9.txt"
-    let fn = TEST_DATA
+    let fn = "./input/day_9.txt"
+    #let fn = TEST_DATA_2
 
     var moveMap = newSeq[tuple[dir: string, amt: int]]()
+    var points = collect(newSeq):
+        for x in 0 ..< 10:
+            Point(x: 0, y: 0)
 
     withStream(f, fn, fmRead):
         for line in lines(f):
             let parts = line.split(" ")
             moveMap.add((dir: parts[0], amt: parseInt(parts[1])))
 
-    echo "Part two: "
+    var tailMoves = initHashSet[Point]()
+    for move in moveMap:
+        for i in 0 ..< move.amt:
+            singleMove(move.dir, points[0], points[1], tailMoves)
+            for p in 1 ..< points.len:
+                if not touching(points[p - 1], points[p]):
+                    if p == points.len - 1:
+                        catchUp(points[p - 1], points[p], tailMoves, true)
+                    else:
+                        catchUp(points[p - 1], points[p], tailMoves)
+
+    echo "Part two: ", tailMoves.len + 1 #count tail starting position
 
 when isMainModule:
     partOne()
